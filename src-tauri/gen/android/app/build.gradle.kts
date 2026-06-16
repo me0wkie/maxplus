28fa1c58913e4f62ffc2327efd3b69c27bab014c
Meowkie <229316276+me0wkie@users.noreply.github.com>
2026-06-16 13:18:31 +0200

    fix minSdk for github build

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "org.meowkie.max"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
